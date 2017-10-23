089ed2b8fb3b7af6325045f289408160ef62227b
Kristina Taneva <tinatanev@gmail.com>
2017-10-23 16:45:13 +0200

    Added distance functionality

--- app/router/routes/traffic.js
+++ app/router/routes/traffic.js
@@ -1,6 +1,8 @@
 /**
  * Created by kristinataneva on 9/6/17.
  */
+const haversine = require('haversine');
+
 module.exports = (app, db) => {
 
     // GET all traffic
@@ -28,16 +30,71 @@ module.exports = (app, db) => {
         const latitude = req.body.latitude;
         const longtitude = req.body.longtitude;
         const address = req.body.address;
+        var checked;
+        const startDistance = {
+            latitude: latitude,
+            longitude: longtitude
+        };
+        var shouldThereBeNewPost;
+        // find all traffic and then check if the coordinates between the new one is close to some of the previous data
 
-        db.traffic.create({
-            name:name,
-            latitude:latitude,
-            longtitude:longtitude,
-            address: address
-        })
-            .then(newTraffic => {
-                res.json(newTraffic)
-            })
+        function DistanceCheck(element){
+            const endDistance = {
+                latitude: element.dataValues.latitude,
+                longitude: element.dataValues.longtitude
+            }
+            const measuredDistance=haversine(startDistance, endDistance, {unit: 'meter'});
+
+            if(measuredDistance<250){
+                console.log("Distance is smaller than 250, no need for another post");
+                return false;
+            }
+            else return true;
+
+        }
+        db.traffic.findAll({ where: {name:name}}).then(traffic=>{
+            //traffic.forEach(function(item){
+            //   //
+            //   //console.log("found traffic with the same name ",item.dataValues.id, item.dataValues.latitude,item.dataValues.longtitude);
+            //   // const endDistance = {
+            //   //     latitude: item.dataValues.latitude,
+            //   //     longitude: item.dataValues.longtitude
+            //   // }
+            //   // const measuredDistance=haversine(startDistance, endDistance, {unit: 'meter'});
+            //   // console.log(measuredDistance," Distance between found traffic with same name ",shouldThereBeNewPost," shouldtherebenewpost");
+            //   // if(measuredDistance<250){
+            //   //     console.log("Distance is smaller than 250, no need for another post");
+            //   //
+            //   //     return ;
+            //   // }
+            //   // else{
+            //   //   //  shouldThereBeNewPost=1;
+            //   //
+
+            //   // }
+            //});
+            checked = traffic.every(DistanceCheck);
+            if(checked === true){
+                console.log("true");
+                db.traffic.create({
+                    name:name,
+                    latitude:latitude,
+                    longtitude:longtitude,
+                    address: address
+                })
+                    .then(newTraffic => {
+                       // res.json(newTraffic)
+                        res.send("Додадена вест");
+                        res.status(200).end();
+
+                    });
+            }
+            else{
+                console.log("false");
+                res.send("Веќе постои таа вест");
+                res.status(400).end();
+            }
+        });
     });
 
     // DELETE a single traffic spot
